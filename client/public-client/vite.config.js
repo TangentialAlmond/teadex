2b9c8d6dfee2de1748272e6872f8377e4d6a9ba1
Amanda Ng <amanda.ng.rh@gmail.com>
2025-12-17 22:28:34 +0800

    feat: prep for deployment of public-client

--- client/public-client/vite.config.js
+++ client/public-client/vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  rserver: {
+    fs: {
+      // Allow Vite to serve files from one level up (the shared folder)
+      allow: ['..']
+    }
+  },
+  resolve: {
+    alias: {
+      // This forces Vite to use the SAME React instance for everyone
+      'react': path.resolve(__dirname, 'node_modules/react'),
+      'react-dom': path.resolve(__dirname, 'node_modules/react-dom')
+    }
+  }
 })
