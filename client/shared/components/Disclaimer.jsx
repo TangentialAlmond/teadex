6145a815d41ef2387254365d0e8afea42ba76528
Amanda Ng <amanda.ng.rh@gmail.com>
2026-01-15 18:03:48 +0800

    style: convert disclaimer from badge to text which is more stable across screen sizes

--- client/shared/components/Disclaimer.jsx
+++ client/shared/components/Disclaimer.jsx
@@ -2,13 +2,10 @@ import { Info } from "lucide-react"
 
 const Disclaimer = () => {
   return (
-    /* flex: Enables flexbox 
-       justify-center: Centers the badge horizontally
-    */
-    <div className="mx-auto max-w-6xl p-4 flex justify-center">
-      <div className="badge badge-default badge-outline h-auto py-2 text-center text-sm italic">
+    <div className="mx-auto max-w-6xl p-4 flex">
+      <div className="h-auto py-2 text-sm italic">
         <span>
-          <strong>Disclaimer:</strong> Entries may contain errors as the TeaDex data is manually curated.
+          <strong>⚠ Disclaimer:</strong> Entries may contain errors as the TeaDex data is manually curated.
         </span>
       </div>
     </div>
