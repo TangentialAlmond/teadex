102015f2d255bad90dc9793797105cdfb8daf95e
Amanda Ng <amanda.ng.rh@gmail.com>
2026-01-15 16:00:55 +0800

    feat: implement NotFoundPage and routing for the public-client

--- client/public-client/src/App.jsx
+++ client/public-client/src/App.jsx
@@ -2,14 +2,17 @@ import { Routes, Route } from "react-router"
 import Navbar from "./components/Navbar"
 import HomePage from "./pages/HomePage"
 import EntryViewPage from "./pages/EntryViewPage"
+import NotFoundPage from "../../shared/pages/NotFoundPage"
 
 const App = () => {
   return (
     <div data-theme="cupcake">
       <Navbar />
+
       <Routes>
         <Route path="/" element={<HomePage />}/>
         <Route path="/entry/:id/view" element={<EntryViewPage />}/>
+        <Route path="*" element={<NotFoundPage />}/>
       </Routes>
     </div>
   )
